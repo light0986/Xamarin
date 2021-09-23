91a7f4a73da7bba74b653c79f4b479c07e21db01
light0986 <82749910+light0986@users.noreply.github.com>
2021-09-23 21:18:48 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,60 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AndroidApp1
+{
+    public class ImageAdapter : BaseAdapter
+    {
+        Context context;
+
+        public ImageAdapter(Context c)
+        {
+            context = c;
+        }
+
+        public override int Count
+        {
+            get { return thumbIds.Length; }
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return null;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return 0;
+        }
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            ImageView imageView;
+
+            if (convertView == null)
+            {
+                imageView = new ImageView(context);
+                imageView.LayoutParameters = new GridView.LayoutParams(200,200);
+                imageView.SetScaleType(ImageView.ScaleType.CenterCrop);
+                imageView.SetPadding(1,1,1,1);
+            }
+            else
+            {
+                imageView = (ImageView)convertView;
+            }
+
+            imageView.SetImageResource(thumbIds[position]);
+            return imageView;
+        }
+
+        int[] thumbIds = {
+         Resource.Drawable.sddefault, Resource.Drawable.image0};
+    }
+}
